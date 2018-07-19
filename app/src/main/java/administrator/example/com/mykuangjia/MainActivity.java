022d11c1df52734ab06dfae2ae4f89a86a56e538
zhaodongliang <1037438704@qq.com>
2018-07-19 16:57:06 +0800

    基本应用

--- app/src/main/java/administrator/example/com/mykuangjia/MainActivity.java
+++ app/src/main/java/administrator/example/com/mykuangjia/MainActivity.java
@@ -1,17 +1,17 @@
 package administrator.example.com.mykuangjia;
 
-import android.os.Bundle;
-
 import administrator.example.com.framing.BaseActivity;
+import administrator.example.com.framing.interfaces.DarkNavigationBarTheme;
+import administrator.example.com.framing.interfaces.DarkStatusBarTheme;
+import administrator.example.com.framing.interfaces.Layout;
+import administrator.example.com.framing.interfaces.NavigationBarBackgroundColor;
 import administrator.example.com.framing.util.JumpParameter;
-
+@Layout(R.layout.activity_main) //布局
+@DarkStatusBarTheme(true) //开启顶部状态栏图标、文字暗色模式
+@NavigationBarBackgroundColor(a = 0, r = 0, g = 0, b = 0)
+//透明颜色   设置底部导航栏背景颜色（a = 255,r = 255,g = 255,b = 255 黑色的）
+@DarkNavigationBarTheme(true) //开启底部导航栏按钮暗色模式
 public class MainActivity extends BaseActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     @Override
     public void initViews() {
